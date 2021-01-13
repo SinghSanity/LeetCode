b76aec4b9282ee0d479e75b7bc60f0a5a4e26da6
Amandeep Singh <40546763+SinghSanity@users.noreply.github.com>
2021-01-13 12:32:09 -0500

    Create solution.py

---
+++ Add Two Numbers/solution.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+def listToNumber(l: ListNode):
+    num = ""
+    curr = l
+    while(True):
+        num = str(curr.val) + num
+        if (curr.next is None):
+            break
+        curr = curr.next
+    return num
+
+def numberToList(num) -> ListNode:
+    l = ListNode(int(num[0]), None)
+    for i in range(1, len(num)):
+        curr = ListNode(int(num[i]), l)
+        l = curr
+    return l
+
+class Solution:    
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        num1 = int(listToNumber(l1))
+        num2 = int(listToNumber(l2))
+        num = num1+num2
+        
+        return numberToList(str(num))
